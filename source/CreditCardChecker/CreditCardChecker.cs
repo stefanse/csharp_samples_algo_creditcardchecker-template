6540bb5926bf1bd0cf4ea46940bfa264ea9d39eb
Stefan Steininger <s.steininger@hotmail.com>
2020-03-26 19:05:48 +0100

    debuging erforderlich ;)

--- source/CreditCardChecker/CreditCardChecker.cs
+++ source/CreditCardChecker/CreditCardChecker.cs
@@ -4,22 +4,68 @@ namespace CreditCardChecker
 {
     public class CreditCardChecker
     {
+       
         /// <summary>
         /// Diese Methode überprüft eine Kreditkartenummer, ob diese gültig ist.
         /// Regeln entsprechend der Angabe.
         /// </summary>
+        ///
+
+
         public static bool IsCreditCardValid(string creditCardNumber)
         {
-            throw new NotImplementedException();
+            int oddSum = 0;
+            int evenSum =0 ;
+
+
+            for (int i = 0; i < creditCardNumber.Length -2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                   evenSum +=  ConvertToInt(creditCardNumber[i]) * 2;
+                   
+                }
+
+                else 
+                {
+                    oddSum += ConvertToInt(creditCardNumber[i]);
+                }
+            }
+
+            if (CalculateCheckDigit(oddSum, evenSum) == creditCardNumber.Length-2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+
+
         }
 
         /// <summary>
         /// Berechnet aus der Summe der geraden Stellen (bereits verdoppelt) und
         /// der Summe der ungeraden Stellen die Checkziffer.
         /// </summary>
+        ///
         private static int CalculateCheckDigit(int oddSum, int evenSum)
         {
-            throw new NotImplementedException();
+            int number;
+            int checksum = 0 ;
+
+            number = CalculateDigitSum(oddSum) + evenSum;
+            
+            while (number % 10 != 0)
+            {
+                number++;
+                checksum++;
+
+            }
+
+            return checksum;
+            
         }
 
         /// <summary>
@@ -27,12 +73,24 @@ namespace CreditCardChecker
         /// </summary>
         private static int CalculateDigitSum(int number)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            int digit = 0;
+            while (number > 0)
+            {
+
+                digit = number % 10;
+                number = number / 10;
+                sum = sum + digit;
+                
+            }
+            return sum;
         }
 
         private static int ConvertToInt(char ch)
         {
-            throw new NotImplementedException();
+            int number;
+            number = ch - '0';
+            return number;
         }
     }
 }
